8b128291d269460eef43a0406ed7c4fb0ad4e2f5
Swapneel Chaudhuri <113018040+swapneel111@users.noreply.github.com>
2024-07-16 12:58:20 +0000

    initial commit

---
+++ COSMOS APP/labo.js
@@ -0,0 +1,253 @@
+var arr=  JSON.parse(localStorage.getItem("cart")) || []
+
+function b1()
+{
+    object = 
+    {
+        name : "Special Health Package Premium",
+        price : 2824 
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function b2()
+{
+    object = 
+    {
+        name : "Compherensive Package - Women",
+        price : 2352
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function b3()
+{
+    object = 
+    {
+        name : "Diabetes And Liquid Profile Premium",
+        price : 822 
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m11()
+{
+    object = 
+    {
+        name : "COVID 19 RTPCR With Home Collection",
+        price : 750 
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m12()
+{
+    object = 
+    {
+        name : "Complete Blooad Count (CBC)",
+        price : 360 
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m13()
+{
+    object = 
+    {
+        name : "C-Reactive Protein",
+        price : 490 
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m14()
+{
+    object = 
+    {
+        name : "Thyroid Profile - Free (FT3, FT4,",
+        price : 880 
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m15()
+{
+    object = 
+    {
+        name : "Liver Function Test (LFT)",
+        price : 595
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m16()
+{
+    object = 
+    {
+        name : "Complete Urine Examination",
+        price : 240
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m21()
+{
+    object = 
+    {
+        name : "Diabetes and Liquid Profile",
+        price : 822
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m22()
+{
+    object = 
+    {
+        name : "Diabetes Screening",
+        price : 469
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m23()
+{
+    object = 
+    {
+        name : "Diabetes Package Basic",
+        price : 588
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m24()
+{
+    object = 
+    {
+        name : "Diabetes Package Advance",
+        price : 1881
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m25()
+{
+    object = 
+    {
+        name : "Liquid Profile",
+        price : 650
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m26()
+{
+    object = 
+    {
+        name : "HbA1c, Glycated Hemoglobin",
+        price : 600
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m31()
+{
+    object = 
+    {
+        name : "Comphernsive Package - Women",
+        price : 2352
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m32()
+{
+    object = 
+    {
+        name : "Comphernsive Package - senior..",
+        price : 2352
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m33()
+{
+    object = 
+    {
+        name : "Women Welness - Advance",
+        price : 3587
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m34()
+{
+    object = 
+    {
+        name : "Women Welness - Basic",
+        price : 2469
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m35()
+{
+    object = 
+    {
+        name : "Thyroid Profile - Free (FT3, FT4,",
+        price : 880
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
+
+function m36()
+{
+    object = 
+    {
+        name : "Haemoglobin",
+        price : 220
+    }
+
+    arr.push(object)
+    localStorage.setItem("cart",JSON.stringify(arr))
+}
